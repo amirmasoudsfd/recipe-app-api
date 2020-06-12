910a8c88640281aca9c3f64571f67576a2bd2478
amir_masoud <amir.masoud.sefidian@gmail.com>
2020-06-12 12:37:06 +0430

    add custom user model

---
+++ app/app/calc.py
@@ -0,0 +1,8 @@
+def add(x, y):
+    """Add two numbers together and return the result"""
+    return x + y
+
+
+def subtract(x, y):
+    """Add two numbers together and return the result"""
+    return y - x
